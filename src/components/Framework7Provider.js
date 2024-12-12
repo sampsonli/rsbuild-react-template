94460efd9e5e5c30fd160c6ae8870a794be95f0a
李春 <lichun5@sfmail.sf-express.com>
2024-12-12 14:07:16 +0800

    dev

---
+++ src/components/Framework7Provider.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'dayjs/locale/zh-cn';
+
+import Framework7 from 'framework7/lite-bundle';
+import Framework7React, {App} from 'framework7-react';
+Framework7.use(Framework7React);
+
+import 'framework7/css/bundle';
+import 'framework7-icons';
+
+// eslint-disable-next-line react/prop-types
+const Framework7Provider = ({children}) => {
+    return (<App theme="md">
+            {children}
+        </App>);
+};
+
+export default Framework7Provider;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {evtBus} from 'mtor';
 import Routes from './routes';
-import CustomerProvider from '~/components/CustomerProvider';
-
+// import AntdProvider from '~/components/AntdProvider';
+import Framework7Provider from '~/components/Framework7Provider';
+import 'dayjs/locale/zh-cn';
+import dayjs from 'dayjs';
+dayjs.locale('zh-cn');
+window.eventBus = evtBus;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <CustomerProvider>
+      <Framework7Provider>
           <Routes/>
-      </CustomerProvider>
+      </Framework7Provider>
   </React.StrictMode>,
 );
