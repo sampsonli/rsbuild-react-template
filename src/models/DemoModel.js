69fc3bc0e2d05be93976e37f3d17d6b023db3a00
lichun <740056710@qq.com>
2025-11-26 18:09:33 +0800

    dev

--- src/models/DemoModel.js
+++ src/models/DemoModel.js
@@ -1,9 +1,26 @@
 import {define, Model} from 'mtor';
 import * as Comlink from 'comlink';
+import {
+    createSolanaRpc,
+    createSolanaRpcSubscriptions,
+    address,
+    generateKeyPair,
+    airdropFactory,
+    lamports, generateKeyPairSigner
+} from '@solana/kit';
 
 @define(module)
 class DemoModel extends Model {
+    /**
+     *
+     * @type {{rpc, rpcSubscriptions}}
+     */
+    static client = undefined;
     loaded = false;
+
+    client;
+
+    balance = 0;
     /**
      * @type {Comlink.Remote<{heavyCalculation: any}>}
      */
@@ -14,13 +31,39 @@ class DemoModel extends Model {
     async add() {
         this.num++;
         let ret = await DemoModel.work_proxy.heavyCalculation(1000);
-        console.log(`computed2:${ret}`);
+        this.num++;
     }
 
-    init() {
+    async init() {
         if (this.loaded) {
             return;
         }
+        if(!DemoModel.client) {
+            DemoModel.client = {
+                rpc: createSolanaRpc('https://api.devnet.solana.com'),
+                rpcSubscriptions: createSolanaRpcSubscriptions('wss://api.devnet.solana.com'),
+            };
+        }
+        const {rpc} = DemoModel.client;
+
+        const myaddr = address('6P4nuQcveqL8TbssGdRh82qjf6SrmERdn8ieM7hfUmse')
+
+        const airdrop = airdropFactory(DemoModel.client);
+        /*await airdrop({
+            recipientAddress: myaddr,
+            lamports: lamports(5_000_000_000n),
+            commitment: 'confirmed',
+        });*/
+        const w = await generateKeyPairSigner();
+        console.log(w);
+
+
+        const {value} = await rpc.getBalance(myaddr).send();
+       // console.log(typeof value)
+        this.balance = Number( value)/ 1e9;
+
+
+
         const work = new Worker(new URL('./mywork.js', import.meta.url));
         DemoModel.work_proxy = Comlink.wrap(work);
         this.onBeforeReset(() => {
--- src/views/Demo/index.jsx
+++ src/views/Demo/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import style from './style.module.less';
 import {useInitModel} from 'mtor';
 import DemoModel from '~/models/DemoModel';
+
 const Demo = () => {
     const model = useInitModel(DemoModel, ({init}) => init());
+
     return (<div className={style.container}>
-        <div className={style.item} onClick={model.add}>{model.num}</div>
-        <div className={style.item}>2</div>
-        <div className={style.item}>{model.num}</div>
-        <div className={style.item}>4</div>
+
+            <div className={style.item} onClick={model.add}>{model.num}</div>
+            <div className={style.item}>{model.balance}</div>
+            <div className={style.item}>{model.num}</div>
+            <div className={style.item}>4</div>
+
+
 
 
     </div>);
